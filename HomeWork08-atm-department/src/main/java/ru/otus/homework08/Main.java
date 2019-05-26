7c2428d677097190aca75524f2bafe2dc12e5675
vintik <poljak181@gmail.com>
2019-05-26 18:29:56 +0300

    Add text for homework08

--- HomeWork08-atm-department/src/main/java/ru/otus/homework08/Main.java
+++ HomeWork08-atm-department/src/main/java/ru/otus/homework08/Main.java
@@ -1,5 +1,13 @@
 package ru.otus.homework08;
 
+// ДЗ 08. ATM Department
+// Написать приложение ATM Department:
+// • Приложение может содержать несколько ATM
+// • Department может собирать сумму остатков со всех ATM
+// • Department может инициировать событие – восстановить состояние всех ATM до начального.
+// (начальные состояния у разных ATM могут быть разными).
+// Можно попробовать использовать Observer, Memento
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("OLE-OLE");
