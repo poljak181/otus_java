f004d18e8e0ad8eba48fa364ab88b0113f938c54
vintik <poljak181@gmail.com>
2019-05-26 18:35:37 +0300

    Add text for homework09

--- HomeWork09-json/src/main/java/ru/otus/homework09/Main.java
+++ HomeWork09-json/src/main/java/ru/otus/homework09/Main.java
@@ -1,5 +1,10 @@
 package ru.otus.homework09;
 
+// ДЗ-09: JSON object writer
+// Напишите свой json object writer (object to JSON string)
+// аналогичный gson на основе javax.json или simple-json и Reflection.
+// Поддержите массивы объектов и примитивных типов, и коллекции из стандартный библиотерки.
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("OLE 09");
